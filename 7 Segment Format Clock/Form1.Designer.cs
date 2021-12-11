294cf0e35d9186b6f757ff4f926395697879ae81
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2021-12-11 12:45:04 +0800

    added button for hours / name changes.

--- 7 Segment Format Clock/Form1.Designer.cs
+++ 7 Segment Format Clock/Form1.Designer.cs
@@ -29,7 +29,155 @@ namespace _7_Segment_Format_Clock
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnRHrs7 = new System.Windows.Forms.Button();
+            this.BtnRHrs5 = new System.Windows.Forms.Button();
+            this.BtnRHrs6 = new System.Windows.Forms.Button();
+            this.BtnRHrs4 = new System.Windows.Forms.Button();
+            this.BtnRHrs3 = new System.Windows.Forms.Button();
+            this.BtnRHrs2 = new System.Windows.Forms.Button();
+            this.BtnRHrs1 = new System.Windows.Forms.Button();
+            this.BtnLHrs7 = new System.Windows.Forms.Button();
+            this.BtnLHrs5 = new System.Windows.Forms.Button();
+            this.BtnLHrs6 = new System.Windows.Forms.Button();
+            this.BtnLHrs4 = new System.Windows.Forms.Button();
+            this.BtnLHrs3 = new System.Windows.Forms.Button();
+            this.BtnLHrs2 = new System.Windows.Forms.Button();
+            this.BtnLHrs1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BtnRHrs7
+            // 
+            this.BtnRHrs7.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnRHrs7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRHrs7.Location = new System.Drawing.Point(179, 223);
+            this.BtnRHrs7.Name = "BtnRHrs7";
+            this.BtnRHrs7.Size = new System.Drawing.Size(62, 22);
+            this.BtnRHrs7.TabIndex = 27;
+            this.BtnRHrs7.UseVisualStyleBackColor = true;
+            // 
+            // BtnRHrs5
+            // 
+            this.BtnRHrs5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRHrs5.Location = new System.Drawing.Point(156, 160);
+            this.BtnRHrs5.Name = "BtnRHrs5";
+            this.BtnRHrs5.Size = new System.Drawing.Size(22, 62);
+            this.BtnRHrs5.TabIndex = 26;
+            this.BtnRHrs5.UseVisualStyleBackColor = true;
+            // 
+            // BtnRHrs6
+            // 
+            this.BtnRHrs6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRHrs6.Location = new System.Drawing.Point(242, 161);
+            this.BtnRHrs6.Name = "BtnRHrs6";
+            this.BtnRHrs6.Size = new System.Drawing.Size(22, 62);
+            this.BtnRHrs6.TabIndex = 25;
+            this.BtnRHrs6.UseVisualStyleBackColor = true;
+            // 
+            // BtnRHrs4
+            // 
+            this.BtnRHrs4.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnRHrs4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRHrs4.Location = new System.Drawing.Point(179, 138);
+            this.BtnRHrs4.Name = "BtnRHrs4";
+            this.BtnRHrs4.Size = new System.Drawing.Size(62, 22);
+            this.BtnRHrs4.TabIndex = 24;
+            this.BtnRHrs4.UseVisualStyleBackColor = true;
+            // 
+            // BtnRHrs3
+            // 
+            this.BtnRHrs3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRHrs3.Location = new System.Drawing.Point(242, 76);
+            this.BtnRHrs3.Name = "BtnRHrs3";
+            this.BtnRHrs3.Size = new System.Drawing.Size(22, 62);
+            this.BtnRHrs3.TabIndex = 23;
+            this.BtnRHrs3.UseVisualStyleBackColor = true;
+            // 
+            // BtnRHrs2
+            // 
+            this.BtnRHrs2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BtnRHrs2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRHrs2.Location = new System.Drawing.Point(156, 76);
+            this.BtnRHrs2.Name = "BtnRHrs2";
+            this.BtnRHrs2.Size = new System.Drawing.Size(22, 62);
+            this.BtnRHrs2.TabIndex = 22;
+            this.BtnRHrs2.UseVisualStyleBackColor = true;
+            // 
+            // BtnRHrs1
+            // 
+            this.BtnRHrs1.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnRHrs1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRHrs1.Location = new System.Drawing.Point(179, 54);
+            this.BtnRHrs1.Name = "BtnRHrs1";
+            this.BtnRHrs1.Size = new System.Drawing.Size(62, 22);
+            this.BtnRHrs1.TabIndex = 21;
+            this.BtnRHrs1.UseVisualStyleBackColor = true;
+            // 
+            // BtnLHrs7
+            // 
+            this.BtnLHrs7.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnLHrs7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLHrs7.Location = new System.Drawing.Point(57, 223);
+            this.BtnLHrs7.Name = "BtnLHrs7";
+            this.BtnLHrs7.Size = new System.Drawing.Size(62, 22);
+            this.BtnLHrs7.TabIndex = 20;
+            this.BtnLHrs7.UseVisualStyleBackColor = true;
+            // 
+            // BtnLHrs5
+            // 
+            this.BtnLHrs5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLHrs5.Location = new System.Drawing.Point(34, 160);
+            this.BtnLHrs5.Name = "BtnLHrs5";
+            this.BtnLHrs5.Size = new System.Drawing.Size(22, 62);
+            this.BtnLHrs5.TabIndex = 19;
+            this.BtnLHrs5.UseVisualStyleBackColor = true;
+            // 
+            // BtnLHrs6
+            // 
+            this.BtnLHrs6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLHrs6.Location = new System.Drawing.Point(120, 161);
+            this.BtnLHrs6.Name = "BtnLHrs6";
+            this.BtnLHrs6.Size = new System.Drawing.Size(22, 62);
+            this.BtnLHrs6.TabIndex = 18;
+            this.BtnLHrs6.UseVisualStyleBackColor = true;
+            // 
+            // BtnLHrs4
+            // 
+            this.BtnLHrs4.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnLHrs4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLHrs4.Location = new System.Drawing.Point(57, 138);
+            this.BtnLHrs4.Name = "BtnLHrs4";
+            this.BtnLHrs4.Size = new System.Drawing.Size(62, 22);
+            this.BtnLHrs4.TabIndex = 17;
+            this.BtnLHrs4.UseVisualStyleBackColor = true;
+            // 
+            // BtnLHrs3
+            // 
+            this.BtnLHrs3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLHrs3.Location = new System.Drawing.Point(120, 76);
+            this.BtnLHrs3.Name = "BtnLHrs3";
+            this.BtnLHrs3.Size = new System.Drawing.Size(22, 62);
+            this.BtnLHrs3.TabIndex = 16;
+            this.BtnLHrs3.UseVisualStyleBackColor = true;
+            // 
+            // BtnLHrs2
+            // 
+            this.BtnLHrs2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BtnLHrs2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLHrs2.Location = new System.Drawing.Point(34, 76);
+            this.BtnLHrs2.Name = "BtnLHrs2";
+            this.BtnLHrs2.Size = new System.Drawing.Size(22, 62);
+            this.BtnLHrs2.TabIndex = 15;
+            this.BtnLHrs2.UseVisualStyleBackColor = true;
+            // 
+            // BtnLHrs1
+            // 
+            this.BtnLHrs1.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnLHrs1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLHrs1.Location = new System.Drawing.Point(57, 54);
+            this.BtnLHrs1.Name = "BtnLHrs1";
+            this.BtnLHrs1.Size = new System.Drawing.Size(62, 22);
+            this.BtnLHrs1.TabIndex = 14;
+            this.BtnLHrs1.UseVisualStyleBackColor = true;
             // 
             // ClckSgmnt
             // 
@@ -37,6 +185,20 @@ namespace _7_Segment_Format_Clock
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(957, 416);
+            this.Controls.Add(this.BtnRHrs7);
+            this.Controls.Add(this.BtnRHrs5);
+            this.Controls.Add(this.BtnRHrs6);
+            this.Controls.Add(this.BtnRHrs4);
+            this.Controls.Add(this.BtnRHrs3);
+            this.Controls.Add(this.BtnRHrs2);
+            this.Controls.Add(this.BtnRHrs1);
+            this.Controls.Add(this.BtnLHrs7);
+            this.Controls.Add(this.BtnLHrs5);
+            this.Controls.Add(this.BtnLHrs6);
+            this.Controls.Add(this.BtnLHrs4);
+            this.Controls.Add(this.BtnLHrs3);
+            this.Controls.Add(this.BtnLHrs2);
+            this.Controls.Add(this.BtnLHrs1);
             this.Name = "ClckSgmnt";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -44,6 +206,21 @@ namespace _7_Segment_Format_Clock
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnRHrs7;
+        private System.Windows.Forms.Button BtnRHrs5;
+        private System.Windows.Forms.Button BtnRHrs6;
+        private System.Windows.Forms.Button BtnRHrs4;
+        private System.Windows.Forms.Button BtnRHrs3;
+        private System.Windows.Forms.Button BtnRHrs2;
+        private System.Windows.Forms.Button BtnRHrs1;
+        private System.Windows.Forms.Button BtnLHrs7;
+        private System.Windows.Forms.Button BtnLHrs5;
+        private System.Windows.Forms.Button BtnLHrs6;
+        private System.Windows.Forms.Button BtnLHrs4;
+        private System.Windows.Forms.Button BtnLHrs3;
+        private System.Windows.Forms.Button BtnLHrs2;
+        private System.Windows.Forms.Button BtnLHrs1;
     }
 }
 
