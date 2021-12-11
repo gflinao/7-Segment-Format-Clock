96376ebbcb06f5b9aa8a439002018039b8a85780
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2021-12-11 15:06:43 +0800

    added buttons for semicolons.

--- 7 Segment Format Clock/Form1.Designer.cs
+++ 7 Segment Format Clock/Form1.Designer.cs
@@ -74,6 +74,10 @@ namespace _7_Segment_Format_Clock
             this.BtnLSec1 = new System.Windows.Forms.Button();
             this.Timerfnc = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BtnRHrs7
@@ -531,9 +535,48 @@ namespace _7_Segment_Format_Clock
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(431, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 56;
-            this.label1.Text = "this is a virus";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(307, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 22);
+            this.button1.TabIndex = 57;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(307, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 22);
+            this.button2.TabIndex = 58;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(596, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 22);
+            this.button3.TabIndex = 59;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(596, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(23, 22);
+            this.button4.TabIndex = 60;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // ClckSgmnt
             // 
@@ -541,6 +584,10 @@ namespace _7_Segment_Format_Clock
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(950, 375);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.BtnRSec7);
             this.Controls.Add(this.BtnRSec5);
@@ -638,6 +685,10 @@ namespace _7_Segment_Format_Clock
         private System.Windows.Forms.Button BtnLSec1;
         private System.Windows.Forms.Timer Timerfnc;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
