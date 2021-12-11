a91a673a91820de6560e41d87a950b28655e099a
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2021-12-11 12:47:48 +0800

    added button for minutes / name changes.

--- 7 Segment Format Clock/Form1.Designer.cs
+++ 7 Segment Format Clock/Form1.Designer.cs
@@ -43,6 +43,20 @@ namespace _7_Segment_Format_Clock
             this.BtnLHrs3 = new System.Windows.Forms.Button();
             this.BtnLHrs2 = new System.Windows.Forms.Button();
             this.BtnLHrs1 = new System.Windows.Forms.Button();
+            this.BtnRMin7 = new System.Windows.Forms.Button();
+            this.BtnRMin5 = new System.Windows.Forms.Button();
+            this.BtnRMin6 = new System.Windows.Forms.Button();
+            this.BtnRMin4 = new System.Windows.Forms.Button();
+            this.BtnRMin3 = new System.Windows.Forms.Button();
+            this.BtnRMin2 = new System.Windows.Forms.Button();
+            this.BtnRMin1 = new System.Windows.Forms.Button();
+            this.BtnLMin7 = new System.Windows.Forms.Button();
+            this.BtnLMin5 = new System.Windows.Forms.Button();
+            this.BtnLMin6 = new System.Windows.Forms.Button();
+            this.BtnLMin4 = new System.Windows.Forms.Button();
+            this.BtnLMin3 = new System.Windows.Forms.Button();
+            this.BtnLMin2 = new System.Windows.Forms.Button();
+            this.BtnLMin1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BtnRHrs7
@@ -179,12 +193,160 @@ namespace _7_Segment_Format_Clock
             this.BtnLHrs1.TabIndex = 14;
             this.BtnLHrs1.UseVisualStyleBackColor = true;
             // 
+            // BtnRMin7
+            // 
+            this.BtnRMin7.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnRMin7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRMin7.Location = new System.Drawing.Point(465, 223);
+            this.BtnRMin7.Name = "BtnRMin7";
+            this.BtnRMin7.Size = new System.Drawing.Size(62, 22);
+            this.BtnRMin7.TabIndex = 41;
+            this.BtnRMin7.UseVisualStyleBackColor = true;
+            // 
+            // BtnRMin5
+            // 
+            this.BtnRMin5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRMin5.Location = new System.Drawing.Point(442, 160);
+            this.BtnRMin5.Name = "BtnRMin5";
+            this.BtnRMin5.Size = new System.Drawing.Size(22, 62);
+            this.BtnRMin5.TabIndex = 40;
+            this.BtnRMin5.UseVisualStyleBackColor = true;
+            // 
+            // BtnRMin6
+            // 
+            this.BtnRMin6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRMin6.Location = new System.Drawing.Point(528, 161);
+            this.BtnRMin6.Name = "BtnRMin6";
+            this.BtnRMin6.Size = new System.Drawing.Size(22, 62);
+            this.BtnRMin6.TabIndex = 39;
+            this.BtnRMin6.UseVisualStyleBackColor = true;
+            // 
+            // BtnRMin4
+            // 
+            this.BtnRMin4.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnRMin4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRMin4.Location = new System.Drawing.Point(465, 138);
+            this.BtnRMin4.Name = "BtnRMin4";
+            this.BtnRMin4.Size = new System.Drawing.Size(62, 22);
+            this.BtnRMin4.TabIndex = 38;
+            this.BtnRMin4.UseVisualStyleBackColor = true;
+            // 
+            // BtnRMin3
+            // 
+            this.BtnRMin3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRMin3.Location = new System.Drawing.Point(528, 76);
+            this.BtnRMin3.Name = "BtnRMin3";
+            this.BtnRMin3.Size = new System.Drawing.Size(22, 62);
+            this.BtnRMin3.TabIndex = 37;
+            this.BtnRMin3.UseVisualStyleBackColor = true;
+            // 
+            // BtnRMin2
+            // 
+            this.BtnRMin2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BtnRMin2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRMin2.Location = new System.Drawing.Point(442, 76);
+            this.BtnRMin2.Name = "BtnRMin2";
+            this.BtnRMin2.Size = new System.Drawing.Size(22, 62);
+            this.BtnRMin2.TabIndex = 36;
+            this.BtnRMin2.UseVisualStyleBackColor = true;
+            // 
+            // BtnRMin1
+            // 
+            this.BtnRMin1.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnRMin1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRMin1.Location = new System.Drawing.Point(465, 54);
+            this.BtnRMin1.Name = "BtnRMin1";
+            this.BtnRMin1.Size = new System.Drawing.Size(62, 22);
+            this.BtnRMin1.TabIndex = 35;
+            this.BtnRMin1.UseVisualStyleBackColor = true;
+            // 
+            // BtnLMin7
+            // 
+            this.BtnLMin7.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnLMin7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLMin7.Location = new System.Drawing.Point(343, 223);
+            this.BtnLMin7.Name = "BtnLMin7";
+            this.BtnLMin7.Size = new System.Drawing.Size(62, 22);
+            this.BtnLMin7.TabIndex = 34;
+            this.BtnLMin7.UseVisualStyleBackColor = true;
+            // 
+            // BtnLMin5
+            // 
+            this.BtnLMin5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLMin5.Location = new System.Drawing.Point(320, 160);
+            this.BtnLMin5.Name = "BtnLMin5";
+            this.BtnLMin5.Size = new System.Drawing.Size(22, 62);
+            this.BtnLMin5.TabIndex = 33;
+            this.BtnLMin5.UseVisualStyleBackColor = true;
+            // 
+            // BtnLMin6
+            // 
+            this.BtnLMin6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLMin6.Location = new System.Drawing.Point(406, 161);
+            this.BtnLMin6.Name = "BtnLMin6";
+            this.BtnLMin6.Size = new System.Drawing.Size(22, 62);
+            this.BtnLMin6.TabIndex = 32;
+            this.BtnLMin6.UseVisualStyleBackColor = true;
+            // 
+            // BtnLMin4
+            // 
+            this.BtnLMin4.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnLMin4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLMin4.Location = new System.Drawing.Point(343, 138);
+            this.BtnLMin4.Name = "BtnLMin4";
+            this.BtnLMin4.Size = new System.Drawing.Size(62, 22);
+            this.BtnLMin4.TabIndex = 31;
+            this.BtnLMin4.UseVisualStyleBackColor = true;
+            // 
+            // BtnLMin3
+            // 
+            this.BtnLMin3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLMin3.Location = new System.Drawing.Point(406, 76);
+            this.BtnLMin3.Name = "BtnLMin3";
+            this.BtnLMin3.Size = new System.Drawing.Size(22, 62);
+            this.BtnLMin3.TabIndex = 30;
+            this.BtnLMin3.UseVisualStyleBackColor = true;
+            // 
+            // BtnLMin2
+            // 
+            this.BtnLMin2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BtnLMin2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLMin2.Location = new System.Drawing.Point(320, 76);
+            this.BtnLMin2.Name = "BtnLMin2";
+            this.BtnLMin2.Size = new System.Drawing.Size(22, 62);
+            this.BtnLMin2.TabIndex = 29;
+            this.BtnLMin2.UseVisualStyleBackColor = true;
+            // 
+            // BtnLMin1
+            // 
+            this.BtnLMin1.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen;
+            this.BtnLMin1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLMin1.Location = new System.Drawing.Point(343, 54);
+            this.BtnLMin1.Name = "BtnLMin1";
+            this.BtnLMin1.Size = new System.Drawing.Size(62, 22);
+            this.BtnLMin1.TabIndex = 28;
+            this.BtnLMin1.UseVisualStyleBackColor = true;
+            // 
             // ClckSgmnt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(957, 416);
+            this.Controls.Add(this.BtnRMin7);
+            this.Controls.Add(this.BtnRMin5);
+            this.Controls.Add(this.BtnRMin6);
+            this.Controls.Add(this.BtnRMin4);
+            this.Controls.Add(this.BtnRMin3);
+            this.Controls.Add(this.BtnRMin2);
+            this.Controls.Add(this.BtnRMin1);
+            this.Controls.Add(this.BtnLMin7);
+            this.Controls.Add(this.BtnLMin5);
+            this.Controls.Add(this.BtnLMin6);
+            this.Controls.Add(this.BtnLMin4);
+            this.Controls.Add(this.BtnLMin3);
+            this.Controls.Add(this.BtnLMin2);
+            this.Controls.Add(this.BtnLMin1);
             this.Controls.Add(this.BtnRHrs7);
             this.Controls.Add(this.BtnRHrs5);
             this.Controls.Add(this.BtnRHrs6);
@@ -221,6 +383,20 @@ namespace _7_Segment_Format_Clock
         private System.Windows.Forms.Button BtnLHrs3;
         private System.Windows.Forms.Button BtnLHrs2;
         private System.Windows.Forms.Button BtnLHrs1;
+        private System.Windows.Forms.Button BtnRMin7;
+        private System.Windows.Forms.Button BtnRMin5;
+        private System.Windows.Forms.Button BtnRMin6;
+        private System.Windows.Forms.Button BtnRMin4;
+        private System.Windows.Forms.Button BtnRMin3;
+        private System.Windows.Forms.Button BtnRMin2;
+        private System.Windows.Forms.Button BtnRMin1;
+        private System.Windows.Forms.Button BtnLMin7;
+        private System.Windows.Forms.Button BtnLMin5;
+        private System.Windows.Forms.Button BtnLMin6;
+        private System.Windows.Forms.Button BtnLMin4;
+        private System.Windows.Forms.Button BtnLMin3;
+        private System.Windows.Forms.Button BtnLMin2;
+        private System.Windows.Forms.Button BtnLMin1;
     }
 }
 
